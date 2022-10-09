0e8e9b9590ab074fce623a088f7bdbd25d23dca5
123ahmed565 <ahmedseragmohamed@gmail.com>
2022-10-09 16:50:08 +0200

    updates

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+// start loading screen (PRELOADER)
+const setup = () => {
+    function getSidebarStateFromLocalStorage() {
+      // if it already there, use it
+      if (window.localStorage.getItem("isSidebarOpen")) {
+        return JSON.parse(window.localStorage.getItem("isSidebarOpen"));
+      }
+  
+      // else return the initial state you want
+      return false;
+    }
+  
+    function setSidebarStateToLocalStorage(value) {
+      window.localStorage.setItem("isSidebarOpen", value);
+    }
+  
+    return {
+      loading: true,
+      isSidebarOpen: getSidebarStateFromLocalStorage(),
+      toggleSidbarMenu() {
+        this.isSidebarOpen = !this.isSidebarOpen;
+        setSidebarStateToLocalStorage(this.isSidebarOpen);
+      },
+      isSettingsPanelOpen: false,
+      isSearchBoxOpen: false,
+    };
+  };
+
+
+
+
+  // flat  button
+  $('.action-button').on('click', function(){
+    $('#overlay').toggleClass('show');
+    $(this).toggleClass('active');
+  });
+
+  // add active colorto aside navbar
+  $('.actinThis').on('click', function(){
+    $(this).addClass('activeBackground');
+  });
