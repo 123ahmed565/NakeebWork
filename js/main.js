668e7ecc381aa18f79f529e440b1b30a7e4a3002
123ahmed565 <ahmedseragmohamed@gmail.com>
2022-10-15 05:04:31 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 
 // start loading screen (PRELOADER)
 const setup = () => {
-    function getSidebarStateFromLocalStorage() {
-      // if it already there, use it
-      if (window.localStorage.getItem("isSidebarOpen")) {
-        return JSON.parse(window.localStorage.getItem("isSidebarOpen"));
-      }
-  
-      // else return the initial state you want
-      return false;
+  function getSidebarStateFromLocalStorage() {
+    // if it already there, use it
+    if (window.localStorage.getItem("isSidebarOpen")) {
+      return JSON.parse(window.localStorage.getItem("isSidebarOpen"));
     }
-  
-    function setSidebarStateToLocalStorage(value) {
-      window.localStorage.setItem("isSidebarOpen", value);
-    }
-  
-    return {
-      loading: true,
-      isSidebarOpen: getSidebarStateFromLocalStorage(),
-      toggleSidbarMenu() {
-        this.isSidebarOpen = !this.isSidebarOpen;
-        setSidebarStateToLocalStorage(this.isSidebarOpen);
-      },
-      isSettingsPanelOpen: false,
-      isSearchBoxOpen: false,
-    };
+
+    // else return the initial state you want
+    return false;
+  }
+
+  function setSidebarStateToLocalStorage(value) {
+    window.localStorage.setItem("isSidebarOpen", value);
+  }
+
+  return {
+    loading: true,
+    isSidebarOpen: getSidebarStateFromLocalStorage(),
+    toggleSidbarMenu() {
+      this.isSidebarOpen = !this.isSidebarOpen;
+      setSidebarStateToLocalStorage(this.isSidebarOpen);
+    },
+    isSettingsPanelOpen: false,
+    isSearchBoxOpen: false,
   };
+};
 
 
 
