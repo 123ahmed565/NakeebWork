958828eadbdff6b852dfe0715303320b6480366c
123ahmed565 <ahmedseragmohamed@gmail.com>
2022-10-31 01:15:25 +0200

    updates

--- public/js/main.js
+++ public/js/main.js
@@ -42,3 +42,18 @@ const setup = () => {
   });
 
 
+  // change image in page color extraction
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+    readURL(this);
+});
